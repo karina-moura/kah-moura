a35385464a4de8e5e813a8dc9273941e6f038a3a
Karina  Moura <83098168+karina-moura@users.noreply.github.com>
2024-02-01 14:24:35 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,6 @@
+numero_1 = float(input("Digite o primeiro número: "))
+numero_2 = float(input("Digite o segundo número: "))
+
+resultado = (numero_1 + numero_2)
+
+print("O resultafdo da soma é: ", resultado)
